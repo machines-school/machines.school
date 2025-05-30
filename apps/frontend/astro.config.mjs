d23bf096786502a6b9af5b8aa1106523f232f37d
athivvat <athivvat@gmail.com>
2025-05-30 14:25:14 +0700

    fix: Mermaid as a client-side rendering

--- apps/frontend/astro.config.mjs
+++ apps/frontend/astro.config.mjs
@@ -5,7 +5,6 @@ import mdx from '@astrojs/mdx';
 import rehypeSlug from 'rehype-slug';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
-import remarkMermaid from 'remark-mermaidjs';
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,6 +15,6 @@ export default defineConfig({
     },
     markdown: {
         rehypePlugins: [rehypeSlug, rehypeKatex],
-        remarkPlugins: [remarkMath, remarkMermaid],
+        remarkPlugins: [remarkMath],
     },
 });
