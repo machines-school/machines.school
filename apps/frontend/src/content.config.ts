71053c58c64ee97cc91f58616521c3fc03ced861
athivvat <athivvat@gmail.com>
2025-06-16 17:20:59 +0700

    feat: courses

--- apps/frontend/src/content.config.ts
+++ apps/frontend/src/content.config.ts
@@ -11,9 +11,36 @@ const blog = defineCollection({
             alt: z.string()
         }).optional()
     })
- });
+});
 
+const course = defineCollection({
+    loader: glob({ base: './src/content/course', pattern: '**/*.{md,mdx}' }),
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        duration: z.string(),
+        level: z.enum(['Beginner', 'Intermediate', 'Advanced']),
+        featuredImage: z.object({
+            url: z.string(),
+            alt: z.string()
+        }).optional()
+    })
+});
 
- export const collections = {
+const theRiseOfIntelligence = defineCollection({
+    loader: glob({ base: './src/content/the-rise-of-intelligence', pattern: '**/*.{md,mdx}' }),
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        featuredImage: z.object({
+            url: z.string(),
+            alt: z.string()
+        }).optional()
+    })
+});
+
+export const collections = {
     blog,
- };
+    course,
+    theRiseOfIntelligence
+};
